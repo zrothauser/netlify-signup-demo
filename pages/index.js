7f43e478cd4ee7958cf242e53c336e1e58d6d15f
Zack Rothauser <zrothauser@gmail.com>
2020-07-22 14:38:42 -0700

    Add auth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import netlifyAuth from '../netlifyAuth.js'
+
 export default function Home() {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
+  let [user, setUser] = useState(null)
+
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user)
+      setUser(user)
+      netlifyAuth.closeModal()
+    })
+  }
+
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(false)
+      setUser(null)
+    })
+  }
+
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user)
+      setUser(user)
+    })
+  }, [loggedIn])
+
   return (
     <div className="container">
       <Head>
@@ -12,9 +40,22 @@ export default function Home() {
 
       <main>
         <Header />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+
+        {loggedIn ? (
+          <div>
+            You are logged in!
+
+            {user && <>Welcome {user?.user_metadata.full_name}!</>}
+            <br />
+            <button onClick={logout}>
+              Log out here.
+            </button>
+          </div>
+        ) : (
+          <button onClick={login}>
+            Log in here.
+          </button>
+        )}
       </main>
 
       <Footer />
